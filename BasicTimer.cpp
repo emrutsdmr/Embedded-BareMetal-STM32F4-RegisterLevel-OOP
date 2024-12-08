f40d0a7987a1386084d3f687a079af004a3ea69e
emtsdmr <emrullahtsdmr@gmail.com>
2024-12-08 10:24:55 +0100

    Refactor BasicTimer class to support both TIM6 and TIM7

--- BasicTimer.cpp
+++ BasicTimer.cpp
@@ -7,19 +7,44 @@
 
 #include "BasicTimer.h"
 
-BasicTimer::BasicTimer(){
-
-  // Enable TIM6 Clock
-  __HAL_RCC_TIM6_CLK_ENABLE();
+BasicTimer::BasicTimer(TIM_TypeDef* timer)
+  : _timer(timer) {
+  // Enable the clock for the selected timer
+  if (_timer == TIM6) {
+    __HAL_RCC_TIM6_CLK_ENABLE();
+  } else if (_timer == TIM7) {
+    __HAL_RCC_TIM7_CLK_ENABLE();
+  }
 
   // Set default period (100 ms)
-  TIM6->PSC = 47999;   // Prescaler: 48 MHz / 48000 = 1 kHz
-  TIM6->ARR = 499;     // Auto-reload: 1 kHz / 500 = 2 Hz (100 ms period)
+  _timer->PSC = 47999;   // Prescaler: 48 MHz / 48000 = 1 kHz
+  _timer->ARR = 499;     // Auto-reload: 1 kHz / 500 = 2 Hz (100 ms period)
 
   // Enable Timer Update Interrupt
-  TIM6->DIER |= TIM_DIER_UIE;
+  _timer->DIER |= TIM_DIER_UIE;
 
   // Enable the timer interrupt in NVIC
-  NVIC_EnableIRQ(TIM6_DAC_IRQn);
-  NVIC_SetPriority(TIM6_DAC_IRQn, 2);
+  if (_timer == TIM6) {
+    NVIC_EnableIRQ(TIM6_DAC_IRQn);
+    NVIC_SetPriority(TIM6_DAC_IRQn, 2);
+  } else if (_timer == TIM7) {
+    NVIC_EnableIRQ(TIM7_IRQn);
+    NVIC_SetPriority(TIM7_IRQn, 2);
+  }
+}
+
+void BasicTimer::setPeriod(uint16_t period_ms) {
+  _timer->ARR = period_ms - 1;
+}
+
+void BasicTimer::enable() {
+  _timer->CR1 |= TIM_CR1_CEN;
+}
+
+void BasicTimer::disable() {
+  _timer->CR1 &= ~TIM_CR1_CEN;
+}
+
+uint16_t BasicTimer::getCounterValue() const {
+  return _timer->CNT;
 }
--- BasicTimer.h
+++ BasicTimer.h
@@ -12,13 +12,16 @@
 
 class BasicTimer {
 public:
-  BasicTimer();
-//  void setPeriod(uint16_t period_ms);       // Set the timer period
-//  void enable();                            // Enable the timer
-//  void disable();                           // Disable the timer
-//  uint16_t getCounterValue() const;         // Get the current counter value
+  BasicTimer(TIM_TypeDef* timer);               // Constructor to initialize TIM6 or TIM7
+//  void setPeriod(uint16_t period_ms);         // Set the timer period
+//  void enable();                              // Enable the timer
+//  void disable();                             // Disable the timer
+//  uint16_t getCounterValue() const;           // Get the current counter value
 //
-//  static void IRQHandler(); // Interrupt handler for the timer
+//  static void IRQHandler(TIM_TypeDef* timer); // Interrupt handler for the timer
+
+private:
+  TIM_TypeDef* _timer;                      // Pointer to the selected timer (TIM6 or TIM7)
 };
 
 #endif /* SRC_BASICTIMER_H_ */
