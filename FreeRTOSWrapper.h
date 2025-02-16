1864dccb5e7822534ae19e1536023660c3dce047
emtsdmr <emrullahtsdmr@gmail.com>
2025-02-16 13:59:12 +0100

    Add FreeRTOSWrapper.h: C++ wrapper for FreeRTOS tasks
    - Implements `Task` class for task management.
    - Provides `start()`, virtual `run()`, and FreeRTOS integration.

---
+++ FreeRTOSWrapper.h
@@ -0,0 +1,34 @@
+/*
+ * FreeRTOSWrapper.h
+ *
+ *  Created on: Feb 16, 2025
+ *      Author: emrullah
+ */
+
+#ifndef SRC_FREERTOSWRAPPER_H_
+#define SRC_FREERTOSWRAPPER_H_
+
+namespace FreeRTOS {
+
+// Task Wrapper
+class Task {
+public:
+  Task(const char* name, uint16_t stackSize, UBaseType_t priority);
+  virtual ~Task();
+
+  void start();
+
+protected:
+  virtual void run() = 0;
+
+private:
+  static void taskFunction(void* pvParameters);
+
+  TaskHandle_t handle = nullptr;
+  const char* taskName;
+  uint16_t stackSize;
+  UBaseType_t priority;
+};
+
+}
+#endif /* SRC_FREERTOSWRAPPER_H_ */
